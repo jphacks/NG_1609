f6ea754fcf23d438fbfc75209a92b50a49c142b6
TomoyaHayakawa <simorgh3196@gmail.com>
2016-11-03 16:28:56 +0900

    プロジェクトの作成

---
+++ ios-app/Tourist/Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tourist
+//
+//  Created by Tomoya Hayakawa on 2016/11/03.
+//  Copyright © 2016年 NG_1609. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
