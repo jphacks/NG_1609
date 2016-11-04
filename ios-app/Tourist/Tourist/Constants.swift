883d3cfed1287c114277666ff0a90505a153863c
TomoyaHayakawa <simorgh3196@gmail.com>
2016-11-04 21:37:33 +0900

    GoogleMapテスト用画面の作成

---
+++ ios-app/Tourist/Tourist/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Tourist
+//
+//  Created by Tomoya Hayakawa on 2016/11/04.
+//  Copyright © 2016年 NG_1609. All rights reserved.
+//
+
+import Foundation
+
+final class Constants {
+    static let GoogleMapAPIKey = "AIzaSyDRA-CakqO_NmeWxCi_0sqqposrBQjSZ-Y"
+}
