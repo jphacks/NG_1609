2bbfb8f3f0fa005fcd5a649795c5d8e3b5773315
TomoyaHayakawa <simorgh3196@gmail.com>
2016-11-06 00:46:58 +0900

    Make PR #22

---
+++ ios-app/Tourist/Tourist/Regions.swift
@@ -0,0 +1,28 @@
+//
+//  Regions.swift
+//  Tourist
+//
+//  Created by Tomoya Hayakawa on 2016/11/06.
+//  Copyright © 2016年 NG_1609. All rights reserved.
+//
+
+import Foundation
+import Himotoki
+
+
+struct Regions {
+
+    var regions: [Region]
+
+}
+
+
+// MARK: - :Decodable
+
+extension Regions: Decodable {
+
+    static func decode(_ e: Extractor) throws -> Self {
+        return try 
+    }
+
+}
